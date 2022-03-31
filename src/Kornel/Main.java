15eb6372235698859ffa768b8b18f4464e64f8e1
CornelisPuchowicz <kornel.puchowicz@gmail.com>
2022-03-31 14:53:16 +0200

    Simple project for displaying regular or millitary time

---
+++ src/Kornel/Main.java
@@ -0,0 +1,20 @@
+package Kornel;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        zegar czas = new zegar();
+        
+        czas.setTime(23,55,55);
+        System.out.println(czas.toString());
+        System.out.println(czas.toMilliteray());
+
+
+
+
+
+
+
+    }
+}
